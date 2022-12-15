0f3dcae1dee886f1e7ef40256f4df5ce4ec9a157
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-12-15 15:35:50 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WFA221215
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblShotCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.White;
+            this.btnStart.Font = new System.Drawing.Font("Snap ITC", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(443, 76);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Let it snow!";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // lblNOdf
+            // 
+            this.lblShotCount.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblShotCount.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblShotCount.Location = new System.Drawing.Point(907, 12);
+            this.lblShotCount.Name = "lblNOdf";
+            this.lblShotCount.Size = new System.Drawing.Size(247, 76);
+            this.lblShotCount.TabIndex = 1;
+            this.lblShotCount.Text = "No#sf: 0";
+            this.lblShotCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 739);
+            this.Controls.Add(this.lblShotCount);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmr;
+        private Button btnStart;
+        private Label lblShotCount;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+using WFA221215.Properties;
+
+namespace WFA221215
+{
+    public partial class Form1 : Form
+    {
+        int shotCount = 0;
+        static Random rnd = new();
+        List<PictureBox> snowflakes = new();
+        bool isTmrRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tmr.Tick += OnTick;
+            btnStart.Click += OnStartButtonClick;
+        }
+
+        private void OnStartButtonClick(object? sender, EventArgs e)
+        {
+            if (isTmrRunning)
+            {
+                isTmrRunning = false;
+                tmr.Stop();
+            }
+            else
+            {
+                isTmrRunning = true;
+                tmr.Start();
+            }
+        }
+
+        private void OnTick(object? sender, EventArgs e)
+        {
+            if (rnd.Next(100) > 70 && snowflakes.Count < 100)
+            {
+                int snfHnW = rnd.Next(20, 100);
+
+                snowflakes.Add(
+                    new PictureBox()
+                    {
+                        Image = Resources.snowflake,
+                        Bounds = new(
+                            x: rnd.Next(this.Width), 
+                            y: rnd.Next(this.Height),
+                            height: snfHnW,
+                            width: snfHnW),
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        BackColor = Color.Transparent,
+                    });
+                snowflakes.Last().Click += OnSnowflakeClick;
+
+                this.Controls.Add(snowflakes.Last());
+            }
+            else
+            {
+                List<PictureBox> toKill = new();
+
+                foreach (var snowflake in snowflakes)
+                {
+                    if (snowflake.Location.Y > this.Height) toKill.Add(snowflake);
+                    else if (rnd.Next(100) < 50)
+                    {
+                        int newY = snowflake.Location.Y + (rnd.Next(20) + 1);
+                        snowflake.Location = new(
+                            x: snowflake.Location.X,
+                            y: newY);
+                    }
+                }
+
+                foreach (var oow in toKill)
+                {
+                    snowflakes.Remove(oow);
+                    oow.Dispose();
+                }
+                toKill.Clear();
+            }
+        }
+
+        private void OnSnowflakeClick(object? sender, EventArgs e)
+        {
+            (sender as PictureBox).Image = Resources.blood;
+            shotCount++;
+            lblShotCount.Text = $"No#sf: {shotCount}";
+
+            if (shotCount == 100)
+            {
+                tmr.Stop();
+                foreach (var snf in snowflakes) snf.Dispose();
+                this.BackgroundImageLayout = ImageLayout.Zoom;
+                this.BackgroundImage = Resources.mxm;
+                btnStart.Visible = false;
+                lblShotCount.Visible = false;
+            }
+        }
+    }
+}
